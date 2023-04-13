8cb5a67f2e21f44911d735ab50230d83b38ca153
Javohir <javohirrajapboyev@gmail.com>
2023-04-13 23:43:17 +0500

    lishniy kodlar olindi HomeController.php dan

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,16 +10,6 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index(){
-        $date = News::query()->select("when")->get();
-//        dd($date);
-        foreach ($date as $value){
-            $ts= strtotime($value->when);
-           $day[]= date('d', $ts);
-           $month=date('M', $ts);
-           $year=date('Y', $ts);
-           $time= date('H:i', $ts);
-        }
-//        dd($day);
         $courses=Courses::all();
         $teachers=Teachers::all();
         $news=News::all();
